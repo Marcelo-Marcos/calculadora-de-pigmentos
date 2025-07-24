18c65a2a45fdab96162d9056d775b324b3587b87
Marcelo-Marcos <109537243+Marcelo-Marcos@users.noreply.github.com>
2025-07-24 13:29:31 -0300

    criando o repositório

---
+++ coral.js
@@ -0,0 +1,282 @@
+let pigmento = "";
+const colorantesCoral = [
+  { nome: "XY1", cor: "linear-gradient(to right, #f7e600, #fff6b0)" }, // YG → XY1
+  { nome: "YE2", cor: "linear-gradient(to right, #F4C430, #FFD666)" }, // YM → YE2
+  { nome: "YE1", cor: "linear-gradient(to right, #D4A017, #FFCC33)" }, // YI → YE1
+  { nome: "YE1", cor: "linear-gradient(to right, #F9A602, #FFD95E)" }, // YR → YE1
+  { nome: "XR1", cor: "linear-gradient(to right, #C41E3A, #FF5C5C)" }, // RB → XR1
+  { nome: "MA1", cor: "linear-gradient(to right, #8B2500, #FF7F50)" }, // RI → MA1
+  { nome: "RE1", cor: "linear-gradient(to right, #b061b0, #e2bde2)" }, // PR → RE1
+  { nome: "OR1", cor: "linear-gradient(to right, #5d6fad, #aab5e1)" }, // BO → OR1
+  { nome: "WH1", cor: "linear-gradient(to right, #3F48CC, #9FA8DA)" }, // BI → WH1
+  { nome: "GR1", cor: "linear-gradient(to right, #2E8B57, #98FB98)" }, // GI → GR1
+  { nome: "YE2", cor: "linear-gradient(to right, #123524, #3A6B47)" }, // GO → YE2
+  { nome: "WH1", cor: "linear-gradient(to right, #FFFFFF, #E0E0E0)" }, // WI → WH1
+  { nome: "BU1", cor: "linear-gradient(to right, #000000, #434343)" }, // CB → BU1
+  { nome: "UM1", cor: "linear-gradient(to right, #ca999d, #e6c1c3)" }  // RY → UM1
+];
+
+const icon = document.getElementById("menu-icon");
+const icon2 = document.getElementById("menu-icon2");
+const fundoModal = document.querySelector(".modal-overlay2")
+const menu = document.getElementById("menu");
+
+icon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+icon.style.display = menu.classList.contains("active") ? "none" : "block";
+icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+fundoModal.style.display = "flex";
+});
+
+icon2.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+icon.style.display = menu.classList.contains("active") ? "none" : "block";
+icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+fundoModal.style.display = "none";
+});
+
+
+function showToast(message, type = "info", customColor = null) {
+  const colors = {
+    success: "linear-gradient(to right, #00b09b, #96c93d)",
+    error: "linear-gradient(to right, #e52d27, #b31217)",
+    info: "linear-gradient(to right, #2196F3, #21CBF3)",
+    warning: "linear-gradient(to right, #f52323, #f76161)",
+  };
+  
+    Toastify({
+      text: message,
+      duration: 4000,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      close: true,
+      backgroundColor: customColor || colors[type] || colors.info,
+    }).showToast();
+  }
+
+
+// Atalhos como o toastify do React:
+function showSuccess(msg) {
+  showToast(msg, "success");
+}
+
+function showError(msg) {
+  showToast(msg, "error");
+}
+
+function showInfo(msg) {
+  showToast(msg, "info");
+}
+
+function showWarning(msg) {
+  showToast(msg, "warning");
+}
+
+function showCustomToast(message, customColor) {
+  showToast(message, null, customColor);
+}
+
+function abrirModal() {
+  document.getElementById("meuModal").style.display = "flex";
+  let legendaColorante = document.getElementById("titulo");
+  
+  colorantesCoral.forEach((suvinil) => {
+    if (pigmento == suvinil.nome) {
+      legendaColorante.textContent = "Colorante: " + pigmento;
+      
+      legendaColorante.style.background = suvinil.cor;
+      legendaColorante.style.webkitBackgroundClip = "text";
+      legendaColorante.style.webkitTextFillColor = "transparent";
+      legendaColorante.style.fontWeight = "bold";
+      legendaColorante.style.webkitTextStroke = "0.5px black";
+    }
+  })
+}
+
+function fecharModal() {
+  document.getElementById("meuModal").style.display = "none";
+}
+
+function colorir() {
+  const container = document.querySelectorAll('.colorante');
+  
+  container.forEach((id) => {
+    let pigmentacoes = id.querySelector(".pigmentacoes");
+    let legendaColorante = id.querySelector("h2");
+    
+    let primeiros2 = pigmentacoes.className.substring(0, 3);
+    
+    colorantesCoral.forEach((suvinil) => {
+      if (primeiros2 == suvinil.nome) {
+        legendaColorante.style.background = suvinil.cor;
+        legendaColorante.style.webkitBackgroundClip = "text";
+        legendaColorante.style.webkitTextFillColor = "transparent";
+        legendaColorante.style.fontWeight = "bold";
+        legendaColorante.style.webkitTextStroke = "0.5px black";
+      }
+    })
+  })
+}
+
+function resultado() {
+  
+  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+  
+  const container = document.querySelectorAll('.colorante');
+
+/*  
+  listaDePigmentos = listaDePigmentos.filter(obj => {
+    obj.totalProducao > 0;
+  }
+  )
+  
+  let listaDePigmentos2 = listaDePigmentos.filter(obj => {
+    obj.totalProducao === 0;
+  }
+  )
+  
+  listaDePigmentos2.push(...listaDePigmentos2);
+  */
+  
+  listaDePigmentos.forEach((item) => {
+    let formulacao = item.resultadoConvercao;
+    
+    let totalPigmentacao = item.totalProducao;
+    
+    let totais = ((formulacao / 6.49350649350649) * totalPigmentacao) + 400;
+    
+    container.forEach((id) => {
+      let pigmentacoes = id.querySelector(".pigmentacoes");
+      let resultado = id.querySelector('p');
+      let legendaColorante = id.querySelector("h2");
+      
+      let primeiros2 = pigmentacoes.className.substring(0, 2);
+      
+      colorantesCoral.forEach((suvinil) => {
+        if (primeiros2 == suvinil.nome) {
+          legendaColorante.style.background = suvinil.cor;
+          legendaColorante.style.webkitBackgroundClip = "text";
+          legendaColorante.style.webkitTextFillColor = "transparent";
+          legendaColorante.style.fontWeight = "bold";
+          legendaColorante.style.webkitTextStroke = "0.5px black";
+          
+          resultado.style.background = suvinil.cor;
+          resultado.style.webkitBackgroundClip = "text";
+          resultado.style.webkitTextFillColor = "transparent";
+          resultado.style.fontWeight = "bold";
+          resultado.style.webkitTextStroke = "0.5px black";
+          
+        }
+      })
+      
+      if (item.id == primeiros2) {
+        pigmentacoes.textContent = "Produções: " + totalPigmentacao;
+        resultado.textContent = "Ideal na máquina: " + totais;
+        
+      }
+    });
+    
+    
+  })
+}
+
+function enviar() {
+  const valor1 = document.getElementById("entrada1").value;
+  const valor2 = document.getElementById("entrada2").value;
+  
+  
+  let elementosDaPagina = { id: " ", totalProducao: "0", resultadoConvercao: "0" };
+  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+  
+  /*
+  listaDePigmentos = listaDePigmentos.filter(obj => {
+    obj.totalProducao > 0;
+  }
+  )
+  
+  let listaDePigmentos2 = listaDePigmentos.filter(obj => {
+    obj.totalProducao === 0;
+  }
+  )
+  
+  listaDePigmentos2.push(...listaDePigmentos2)
+  */
+  
+  elementosDaPagina.id = pigmento;
+  elementosDaPagina.resultadoConvercao = valor1;
+  elementosDaPagina.totalProducao = valor2;
+  
+  listaDePigmentos = atualizarItem(listaDePigmentos, elementosDaPagina)
+  
+  
+  localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
+  
+  resultado();
+  fecharModal();
+  colorantesCoral.forEach((suvinil) => {
+    if (pigmento == suvinil.nome) {
+      
+      showCustomToast("Fórmula do colorante " + suvinil.nome + " registrada com sucesso", suvinil.cor).style.webkitTextStroke = "0.5px black";
+      
+    }
+  })
+  
+}
+
+function atualizarItem(lista, novoItem, chave = 'id') {
+  const index = lista.findIndex(item => item[chave] === novoItem[chave]);
+  
+  if (index !== -1) {
+    // Substitui o item existente
+    lista[index] = novoItem;
+  } else {
+    // Adiciona o item se não existir
+    lista.push(novoItem);
+  }
+  
+  return lista;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  colorir();
+  resultado();
+});
+
+const container = document.querySelectorAll('.colorante'); // seletor mais específico
+
+
+container.forEach(item => {
+  item.addEventListener('click', event => {
+    event.stopPropagation();
+    pigmento = event.currentTarget.id;
+    abrirModal(); // apenas se existir
+  });
+});
+
+let icone = document.querySelectorAll(".botao");
+
+icone.forEach(item => {
+  
+  item.addEventListener("click", (event) => {
+    event.stopPropagation();
+    
+    let idDoPai = event.currentTarget;
+    
+    let idPigmento = idDoPai.closest("div");
+    
+    let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+    
+    listaDePigmentos = listaDePigmentos.filter(obj => obj.id !== idPigmento.id);
+    
+    localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
+    
+    showWarning("Excluindo registro!");
+    
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+  })
+});
---
+++ main.js
@@ -0,0 +1,282 @@
+let pigmento = "";
+const colorantesSuvinil = [
+  { nome: "YG", cor: "linear-gradient(to right, #f7e600, #fff6b0)" },
+  { nome: "YM", cor: "linear-gradient(to right, #F4C430, #FFD666)" },
+  { nome: "YI", cor: "linear-gradient(to right, #D4A017, #FFCC33)" },
+  { nome: "YR", cor: "linear-gradient(to right, #F9A602, #FFD95E)" },
+  { nome: "RB", cor: "linear-gradient(to right, #C41E3A, #FF5C5C)" },
+  { nome: "RI", cor: "linear-gradient(to right, #8B2500, #FF7F50)" },
+  { nome: "PR", cor: "linear-gradient(to right, #b061b0, #e2bde2)" },
+  { nome: "BO", cor: "linear-gradient(to right, #5d6fad, #aab5e1)" },
+  { nome: "BI", cor: "linear-gradient(to right, #3F48CC, #9FA8DA)" },
+  { nome: "GI", cor: "linear-gradient(to right, #2E8B57, #98FB98)" },
+  { nome: "GO", cor: "linear-gradient(to right, #123524, #3A6B47)" },
+  { nome: "WI", cor: "linear-gradient(to right, #FFFFFF, #E0E0E0)" },
+  { nome: "CB", cor: "linear-gradient(to right, #000000, #434343)" },
+  { nome: "RY", cor: "linear-gradient(to right, #ca999d, #e6c1c3)"},
+];
+
+const icon = document.getElementById("menu-icon");
+const icon2 = document.getElementById("menu-icon2");
+const fundoModal = document.querySelector(".modal-overlay2")
+const menu = document.getElementById("menu");
+
+icon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+icon.style.display = menu.classList.contains("active") ? "none" : "block";
+icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+fundoModal.style.display = "flex";
+});
+
+icon2.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+icon.style.display = menu.classList.contains("active") ? "none" : "block";
+icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+fundoModal.style.display = "none";
+});
+
+
+function showToast(message, type = "info", customColor = null) {
+  const colors = {
+    success: "linear-gradient(to right, #00b09b, #96c93d)",
+    error: "linear-gradient(to right, #e52d27, #b31217)",
+    info: "linear-gradient(to right, #2196F3, #21CBF3)",
+    warning: "linear-gradient(to right, #f52323, #f76161)",
+  };
+  
+    Toastify({
+      text: message,
+      duration: 4000,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      close: true,
+      backgroundColor: customColor || colors[type] || colors.info,
+    }).showToast();
+  }
+
+
+// Atalhos como o toastify do React:
+function showSuccess(msg) {
+  showToast(msg, "success");
+}
+
+function showError(msg) {
+  showToast(msg, "error");
+}
+
+function showInfo(msg) {
+  showToast(msg, "info");
+}
+
+function showWarning(msg) {
+  showToast(msg, "warning");
+}
+
+function showCustomToast(message, customColor) {
+  showToast(message, null, customColor);
+}
+
+function abrirModal() {
+  document.getElementById("meuModal").style.display = "flex";
+  let legendaColorante = document.getElementById("titulo");
+  
+  colorantesSuvinil.forEach((suvinil) => {
+    if (pigmento == suvinil.nome) {
+      legendaColorante.textContent = "Colorante: " + pigmento;
+      
+      legendaColorante.style.background = suvinil.cor;
+      legendaColorante.style.webkitBackgroundClip = "text";
+      legendaColorante.style.webkitTextFillColor = "transparent";
+      legendaColorante.style.fontWeight = "bold";
+      legendaColorante.style.webkitTextStroke = "0.5px black";
+    }
+  })
+}
+
+function fecharModal() {
+  document.getElementById("meuModal").style.display = "none";
+}
+
+function colorir() {
+  const container = document.querySelectorAll('.colorante');
+  
+  container.forEach((id) => {
+    let pigmentacoes = id.querySelector(".pigmentacoes");
+    let legendaColorante = id.querySelector("h2");
+    
+    let primeiros2 = pigmentacoes.className.substring(0, 2);
+    
+    colorantesSuvinil.forEach((suvinil) => {
+      if (primeiros2 == suvinil.nome) {
+        legendaColorante.style.background = suvinil.cor;
+        legendaColorante.style.webkitBackgroundClip = "text";
+        legendaColorante.style.webkitTextFillColor = "transparent";
+        legendaColorante.style.fontWeight = "bold";
+        legendaColorante.style.webkitTextStroke = "0.5px black";
+      }
+    })
+  })
+}
+
+function resultado() {
+  
+  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+  
+  const container = document.querySelectorAll('.colorante');
+
+/*  
+  listaDePigmentos = listaDePigmentos.filter(obj => {
+    obj.totalProducao > 0;
+  }
+  )
+  
+  let listaDePigmentos2 = listaDePigmentos.filter(obj => {
+    obj.totalProducao === 0;
+  }
+  )
+  
+  listaDePigmentos2.push(...listaDePigmentos2);
+  */
+  
+  listaDePigmentos.forEach((item) => {
+    let formulacao = item.resultadoConvercao;
+    
+    let totalPigmentacao = item.totalProducao;
+    
+    let totais = ((formulacao / 6.49350649350649) * totalPigmentacao) + 400;
+    
+    container.forEach((id) => {
+      let pigmentacoes = id.querySelector(".pigmentacoes");
+      let resultado = id.querySelector('p');
+      let legendaColorante = id.querySelector("h2");
+      
+      let primeiros2 = pigmentacoes.className.substring(0, 2);
+      
+      colorantesSuvinil.forEach((suvinil) => {
+        if (primeiros2 == suvinil.nome) {
+          legendaColorante.style.background = suvinil.cor;
+          legendaColorante.style.webkitBackgroundClip = "text";
+          legendaColorante.style.webkitTextFillColor = "transparent";
+          legendaColorante.style.fontWeight = "bold";
+          legendaColorante.style.webkitTextStroke = "0.5px black";
+          
+          resultado.style.background = suvinil.cor;
+          resultado.style.webkitBackgroundClip = "text";
+          resultado.style.webkitTextFillColor = "transparent";
+          resultado.style.fontWeight = "bold";
+          resultado.style.webkitTextStroke = "0.5px black";
+          
+        }
+      })
+      
+      if (item.id == primeiros2) {
+        pigmentacoes.textContent = "Produções: " + totalPigmentacao;
+        resultado.textContent = "Ideal na máquina: " + totais;
+        
+      }
+    });
+    
+    
+  })
+}
+
+function enviar() {
+  const valor1 = document.getElementById("entrada1").value;
+  const valor2 = document.getElementById("entrada2").value;
+  
+  
+  let elementosDaPagina = { id: " ", totalProducao: "0", resultadoConvercao: "0" };
+  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+  
+  /*
+  listaDePigmentos = listaDePigmentos.filter(obj => {
+    obj.totalProducao > 0;
+  }
+  )
+  
+  let listaDePigmentos2 = listaDePigmentos.filter(obj => {
+    obj.totalProducao === 0;
+  }
+  )
+  
+  listaDePigmentos2.push(...listaDePigmentos2)
+  */
+  
+  elementosDaPagina.id = pigmento;
+  elementosDaPagina.resultadoConvercao = valor1;
+  elementosDaPagina.totalProducao = valor2;
+  
+  listaDePigmentos = atualizarItem(listaDePigmentos, elementosDaPagina)
+  
+  
+  localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
+  
+  resultado();
+  fecharModal();
+  colorantesSuvinil.forEach((suvinil) => {
+    if (pigmento == suvinil.nome) {
+      
+      showCustomToast("Fórmula do colorante " + suvinil.nome + " registrada com sucesso", suvinil.cor).style.webkitTextStroke = "0.5px black";
+      
+    }
+  })
+  
+}
+
+function atualizarItem(lista, novoItem, chave = 'id') {
+  const index = lista.findIndex(item => item[chave] === novoItem[chave]);
+  
+  if (index !== -1) {
+    // Substitui o item existente
+    lista[index] = novoItem;
+  } else {
+    // Adiciona o item se não existir
+    lista.push(novoItem);
+  }
+  
+  return lista;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  colorir();
+  resultado();
+});
+
+const container = document.querySelectorAll('.colorante'); // seletor mais específico
+
+
+container.forEach(item => {
+  item.addEventListener('click', event => {
+    event.stopPropagation();
+    pigmento = event.currentTarget.id;
+    abrirModal(); // apenas se existir
+  });
+});
+
+let icone = document.querySelectorAll(".botao");
+
+icone.forEach(item => {
+  
+  item.addEventListener("click", (event) => {
+    event.stopPropagation();
+    
+    let idDoPai = event.currentTarget;
+    
+    let idPigmento = idDoPai.closest("div");
+    
+    let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
+    
+    listaDePigmentos = listaDePigmentos.filter(obj => obj.id !== idPigmento.id);
+    
+    localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
+    
+    showWarning("Excluindo registro!");
+    
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+  })
+});
