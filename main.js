610ce8b717254aa5aee13014c5d89eb86f44be25
Marcelo Marcos <marcelomarcos.g9@gmail.com>
2025-07-25 06:23:43 -0300

    Melhorias ao excluir dados

--- main.js
+++ main.js
@@ -13,30 +13,29 @@ const colorantesSuvinil = [
   { nome: "GO", cor: "linear-gradient(to right, #123524, #3A6B47)" },
   { nome: "WI", cor: "linear-gradient(to right, #FFFFFF, #E0E0E0)" },
   { nome: "CB", cor: "linear-gradient(to right, #000000, #434343)" },
-  { nome: "RY", cor: "linear-gradient(to right, #ca999d, #e6c1c3)"},
+  { nome: "RY", cor: "linear-gradient(to right, #ca999d, #e6c1c3)" },
 ];
 
 const icon = document.getElementById("menu-icon");
 const icon2 = document.getElementById("menu-icon2");
-const fundoModal = document.querySelector(".modal-overlay2")
+const fundoModal = document.querySelector(".modal-overlay2");
 const menu = document.getElementById("menu");
 
 icon.addEventListener("click", () => {
   menu.classList.toggle("active");
 
-icon.style.display = menu.classList.contains("active") ? "none" : "block";
-icon2.style.display = menu.classList.contains("active") ? "block" : "none";
-fundoModal.style.display = "flex";
+  icon.style.display = menu.classList.contains("active") ? "none" : "block";
+  icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+  fundoModal.style.display = "flex";
 });
 
 icon2.addEventListener("click", () => {
   menu.classList.toggle("active");
 
-icon.style.display = menu.classList.contains("active") ? "none" : "block";
-icon2.style.display = menu.classList.contains("active") ? "block" : "none";
-fundoModal.style.display = "none";
+  icon.style.display = menu.classList.contains("active") ? "none" : "block";
+  icon2.style.display = menu.classList.contains("active") ? "block" : "none";
+  fundoModal.style.display = "none";
 });
-
 
 function showToast(message, type = "info", customColor = null) {
   const colors = {
@@ -45,18 +44,17 @@ function showToast(message, type = "info", customColor = null) {
     info: "linear-gradient(to right, #2196F3, #21CBF3)",
     warning: "linear-gradient(to right, #f52323, #f76161)",
   };
-  
-    Toastify({
-      text: message,
-      duration: 4000,
-      gravity: "top",
-      position: "right",
-      stopOnFocus: true,
-      close: true,
-      backgroundColor: customColor || colors[type] || colors.info,
-    }).showToast();
-  }
 
+  Toastify({
+    text: message,
+    duration: 4000,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    close: true,
+    backgroundColor: customColor || colors[type] || colors.info,
+  }).showToast();
+}
 
 // Atalhos como o toastify do React:
 function showSuccess(msg) {
@@ -82,18 +80,18 @@ function showCustomToast(message, customColor) {
 function abrirModal() {
   document.getElementById("meuModal").style.display = "flex";
   let legendaColorante = document.getElementById("titulo");
-  
+
   colorantesSuvinil.forEach((suvinil) => {
     if (pigmento == suvinil.nome) {
       legendaColorante.textContent = "Colorante: " + pigmento;
-      
+
       legendaColorante.style.background = suvinil.cor;
       legendaColorante.style.webkitBackgroundClip = "text";
       legendaColorante.style.webkitTextFillColor = "transparent";
       legendaColorante.style.fontWeight = "bold";
       legendaColorante.style.webkitTextStroke = "0.5px black";
     }
-  })
+  });
 }
 
 function fecharModal() {
@@ -101,14 +99,14 @@ function fecharModal() {
 }
 
 function colorir() {
-  const container = document.querySelectorAll('.colorante');
-  
+  const container = document.querySelectorAll(".colorante");
+
   container.forEach((id) => {
     let pigmentacoes = id.querySelector(".pigmentacoes");
     let legendaColorante = id.querySelector("h2");
-    
+
     let primeiros2 = pigmentacoes.className.substring(0, 2);
-    
+
     colorantesSuvinil.forEach((suvinil) => {
       if (primeiros2 == suvinil.nome) {
         legendaColorante.style.background = suvinil.cor;
@@ -117,17 +115,17 @@ function colorir() {
         legendaColorante.style.fontWeight = "bold";
         legendaColorante.style.webkitTextStroke = "0.5px black";
       }
-    })
-  })
+    });
+  });
 }
 
 function resultado() {
-  
-  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
-  
-  const container = document.querySelectorAll('.colorante');
+  let listaDePigmentos =
+    JSON.parse(localStorage.getItem("@listaDePigmentos")) || [];
 
-/*  
+  const container = document.querySelectorAll(".colorante");
+
+  /*  
   listaDePigmentos = listaDePigmentos.filter(obj => {
     obj.totalProducao > 0;
   }
@@ -140,21 +138,21 @@ function resultado() {
   
   listaDePigmentos2.push(...listaDePigmentos2);
   */
-  
+
   listaDePigmentos.forEach((item) => {
     let formulacao = item.resultadoConvercao;
-    
+
     let totalPigmentacao = item.totalProducao;
-    
-    let totais = ((formulacao / 6.49350649350649) * totalPigmentacao) + 400;
-    
+
+    let totais = (formulacao / 6.49350649350649) * totalPigmentacao + 400;
+
     container.forEach((id) => {
       let pigmentacoes = id.querySelector(".pigmentacoes");
-      let resultado = id.querySelector('p');
+      let resultado = id.querySelector("p");
       let legendaColorante = id.querySelector("h2");
-      
+
       let primeiros2 = pigmentacoes.className.substring(0, 2);
-      
+
       colorantesSuvinil.forEach((suvinil) => {
         if (primeiros2 == suvinil.nome) {
           legendaColorante.style.background = suvinil.cor;
@@ -162,35 +160,35 @@ function resultado() {
           legendaColorante.style.webkitTextFillColor = "transparent";
           legendaColorante.style.fontWeight = "bold";
           legendaColorante.style.webkitTextStroke = "0.5px black";
-          
+
           resultado.style.background = suvinil.cor;
           resultado.style.webkitBackgroundClip = "text";
           resultado.style.webkitTextFillColor = "transparent";
           resultado.style.fontWeight = "bold";
           resultado.style.webkitTextStroke = "0.5px black";
-          
         }
-      })
-      
+      });
+
       if (item.id == primeiros2) {
         pigmentacoes.textContent = "Produções: " + totalPigmentacao;
         resultado.textContent = "Ideal na máquina: " + totais;
-        
       }
     });
-    
-    
-  })
+  });
 }
 
 function enviar() {
   const valor1 = document.getElementById("entrada1").value;
   const valor2 = document.getElementById("entrada2").value;
-  
-  
-  let elementosDaPagina = { id: " ", totalProducao: "0", resultadoConvercao: "0" };
-  let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
-  
+
+  let elementosDaPagina = {
+    id: " ",
+    totalProducao: "0",
+    resultadoConvercao: "0",
+  };
+  let listaDePigmentos =
+    JSON.parse(localStorage.getItem("@listaDePigmentos")) || [];
+
   /*
   listaDePigmentos = listaDePigmentos.filter(obj => {
     obj.totalProducao > 0;
@@ -204,31 +202,30 @@ function enviar() {
   
   listaDePigmentos2.push(...listaDePigmentos2)
   */
-  
+
   elementosDaPagina.id = pigmento;
   elementosDaPagina.resultadoConvercao = valor1;
   elementosDaPagina.totalProducao = valor2;
-  
-  listaDePigmentos = atualizarItem(listaDePigmentos, elementosDaPagina)
-  
-  
-  localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
-  
+
+  listaDePigmentos = atualizarItem(listaDePigmentos, elementosDaPagina);
+
+  localStorage.setItem("@listaDePigmentos", JSON.stringify(listaDePigmentos));
+
   resultado();
   fecharModal();
   colorantesSuvinil.forEach((suvinil) => {
     if (pigmento == suvinil.nome) {
-      
-      showCustomToast("Fórmula do colorante " + suvinil.nome + " registrada com sucesso", suvinil.cor).style.webkitTextStroke = "0.5px black";
-      
+      showCustomToast(
+        "Fórmula do colorante " + suvinil.nome + " registrada com sucesso",
+        suvinil.cor
+      ).style.webkitTextStroke = "0.5px black";
     }
-  })
-  
+  });
 }
 
-function atualizarItem(lista, novoItem, chave = 'id') {
-  const index = lista.findIndex(item => item[chave] === novoItem[chave]);
-  
+function atualizarItem(lista, novoItem, chave = "id") {
+  const index = lista.findIndex((item) => item[chave] === novoItem[chave]);
+
   if (index !== -1) {
     // Substitui o item existente
     lista[index] = novoItem;
@@ -236,7 +233,7 @@ function atualizarItem(lista, novoItem, chave = 'id') {
     // Adiciona o item se não existir
     lista.push(novoItem);
   }
-  
+
   return lista;
 }
 
@@ -245,11 +242,10 @@ document.addEventListener("DOMContentLoaded", () => {
   resultado();
 });
 
-const container = document.querySelectorAll('.colorante'); // seletor mais específico
+const container = document.querySelectorAll(".colorante"); // seletor mais específico
 
-
-container.forEach(item => {
-  item.addEventListener('click', event => {
+container.forEach((item) => {
+  item.addEventListener("click", (event) => {
     event.stopPropagation();
     pigmento = event.currentTarget.id;
     abrirModal(); // apenas se existir
@@ -258,25 +254,82 @@ container.forEach(item => {
 
 let icone = document.querySelectorAll(".botao");
 
-icone.forEach(item => {
-  
+icone.forEach((item) => {
   item.addEventListener("click", (event) => {
     event.stopPropagation();
-    
+
     let idDoPai = event.currentTarget;
-    
+
     let idPigmento = idDoPai.closest("div");
-    
-    let listaDePigmentos = JSON.parse(localStorage.getItem('@listaDePigmentos')) || [];
-    
-    listaDePigmentos = listaDePigmentos.filter(obj => obj.id !== idPigmento.id);
-    
-    localStorage.setItem('@listaDePigmentos', JSON.stringify(listaDePigmentos));
-    
+
+    let listaDePigmentos =
+      JSON.parse(localStorage.getItem("@listaDePigmentos")) || [];
+
+    listaDePigmentos = listaDePigmentos.filter(
+      (obj) => obj.id !== idPigmento.id
+    );
+
+    localStorage.setItem("@listaDePigmentos", JSON.stringify(listaDePigmentos));
+
     showWarning("Excluindo registro!");
-    
+
     setTimeout(() => {
       location.reload();
     }, 1000);
-  })
+  });
+});
+
+const checkboxes = document.querySelectorAll(".escolha");
+const legenda = document.querySelector(".legenda");
+const iconeExcluir = document.getElementById("botao-cabecalho");
+
+// Exibe ou oculta o botão conforme houver algum marcado
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    // Se pelo menos um estiver marcado, mostra o botão
+    const algumMarcado = Array.from(checkboxes).some((cb) => cb.checked);
+    iconeExcluir.style.display = algumMarcado ? "block" : "none";
+
+    // Verifica se há algum marcado
+    const checkboxesMarcados = Array.from(checkboxes).filter(
+      (cb) => cb.checked
+    );
+    const algumMarcado2 = checkboxesMarcados.length > 0;
+
+    // Mostra ou esconde o botão de exclusão
+    iconeExcluir.style.display = algumMarcado2 ? "block" : "none";
+
+    // Atualiza o número de itens marcados na legenda
+    legenda.textContent = checkboxesMarcados.length;
+  });
+});
+
+iconeExcluir.addEventListener("click", () => {
+  // 1. Pega os IDs dos pais dos checkboxes marcados
+  const idsParaExcluir = Array.from(checkboxes)
+    .filter((cb) => cb.checked)
+    .map((cb) => cb.closest("div").id);
+
+  // 2. Pega a lista atual do localStorage
+  const listaAtual =
+    JSON.parse(localStorage.getItem("@listaDePigmentos")) || [];
+
+  // 3. Filtra removendo os IDs marcados
+  const novaLista = listaAtual.filter(
+    (pigmento) => !idsParaExcluir.includes(pigmento.id)
+  );
+
+  // 4. Atualiza o localStorage
+  localStorage.setItem("@listaDePigmentos", JSON.stringify(novaLista));
+
+  // 5. Atualiza legenda e recarrega
+  legenda.textContent = novaLista.length;
+
+  showWarning("Excluindo registros!");
+
+  setTimeout(() => {
+    location.reload();
+  }, 1000);
 });
